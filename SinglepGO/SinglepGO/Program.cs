b4f0ef490c6a3f848413650a6f14eb84fef6330b
IamNotVerySmart <93706080+IamNotVerySmart@users.noreply.github.com>
2023-10-24 19:19:07 +0200

    Nie działa
    nie działa

---
+++ SinglepGO/SinglepGO/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using SinglepGO;
+
+Board board = new Board();
+
+
+
+while (board.isGameOn)
+{
+    //board.MoveEnemyPosition();
+    board.generateBoard();
+
+}
+/***********************************************************
+ *
+ *      Trzeba dodać jeszcze real-time poruszanie 
+ *      oraz dać możliwość 2 ruchów z czekaniem na ruch gracza
+ *      bo narazie nie da sie tego wygrać.
+ *
+ **********************************************************/
